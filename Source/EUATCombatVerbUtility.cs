051b085051c02e700737e07629f0ed4d0bed3a6a
Eli Fry <fryjah07@gmail.com>
2026-08-05 21:47:40 -0400

    Use verb metadata for apparel opportunity scoring

--- Source/EUATCombatVerbUtility.cs
+++ Source/EUATCombatVerbUtility.cs
@@ -19,11 +19,6 @@ namespace EnemiesUseApparelToo
             "GetShootingTargetScore",
             new[] { typeof(IAttackTarget), typeof(IAttackTargetSearcher), typeof(Verb) });
 
-        private static readonly MethodInfo ValidAoeAffectedTargetMethod = AccessTools.Method(
-            typeof(Ability),
-            "ValidAOEAffectedTarget",
-            new[] { typeof(Thing) });
-
         public static bool TryMakeSmartApparelJob(Pawn pawn, Thing enemyTarget, out Job job)
         {
             job = null;
@@ -62,63 +57,80 @@ namespace EnemiesUseApparelToo
             return verb != null &&
                 verb.Available() &&
                 verb.verbProps?.violent == true &&
-                TryGetAbility(verb, out Ability ability) &&
-                ability.CanCast &&
-                TryGetExplosionRadius(ability, out float radius) &&
+                TryGetExplosionRadius(verb, out float radius) &&
                 radius > 0f;
         }
 
-        private static bool TryEvaluateApparelVerb(Pawn pawn, Thing enemyTarget, Verb verb, out ApparelVerbEvaluation evaluation)
+        private static bool TryEvaluateApparelVerb(Pawn pawn, Thing enemyTarget, Verb verb, out ApparelVerbEvaluation bestEvaluation)
         {
-            evaluation = ApparelVerbEvaluation.Invalid;
+            bestEvaluation = ApparelVerbEvaluation.Invalid;
 
-            if (!TryGetAbility(verb, out Ability ability) || !TryGetExplosionRadius(ability, out float radius))
+            if (!TryGetExplosionRadius(verb, out float radius))
             {
                 return false;
             }
 
-            if (!TryGetVanillaAoeTarget(ability, verb, out LocalTargetInfo castTarget, out Thing affectedTarget))
+            IAttackTarget currentTarget = enemyTarget as IAttackTarget;
+            if (currentTarget != null)
             {
-                return false;
+                TryEvaluateTarget(pawn, verb, currentTarget, radius, ref bestEvaluation);
             }
 
-            float opportunityScore = GetAoeOpportunityScore(pawn, ability, castTarget, radius);
-            if (opportunityScore < MinimumOpportunityScore)
+            foreach (IAttackTarget attackTarget in pawn.Map.attackTargetsCache.GetPotentialTargetsFor(pawn))
             {
-                return false;
+                if (attackTarget == currentTarget)
+                {
+                    continue;
+                }
+
+                TryEvaluateTarget(pawn, verb, attackTarget, radius, ref bestEvaluation);
             }
 
-            IAttackTarget attackTarget = affectedTarget as IAttackTarget ?? enemyTarget as IAttackTarget;
-            float score = GetVanillaShootingTargetScore(attackTarget, pawn, verb, radius) + opportunityScore;
-            evaluation = new ApparelVerbEvaluation(ability, verb, castTarget, score);
-            return true;
+            return bestEvaluation.IsValid;
         }
 
-        private static bool TryGetVanillaAoeTarget(Ability ability, Verb verb, out LocalTargetInfo castTarget, out Thing affectedTarget)
+        private static void TryEvaluateTarget(
+            Pawn pawn,
+            Verb verb,
+            IAttackTarget attackTarget,
+            float radius,
+            ref ApparelVerbEvaluation bestEvaluation)
         {
-            affectedTarget = null;
-            LocalTargetInfo vanillaTarget = ability.AIGetAOETarget();
-            if (!vanillaTarget.IsValid)
+            Thing targetThing = attackTarget.Thing;
+            if (targetThing == null || targetThing.Destroyed || targetThing.Map != pawn.Map || attackTarget.ThreatDisabled(pawn))
             {
-                castTarget = LocalTargetInfo.Invalid;
-                return false;
+                return;
             }
 
-            affectedTarget = vanillaTarget.Thing;
-            if (CanUseTarget(ability, verb, vanillaTarget))
+            if (!TryGetCastTarget(verb, targetThing, out LocalTargetInfo castTarget))
             {
-                castTarget = vanillaTarget;
+                return;
+            }
+
+            float opportunityScore = GetAoeOpportunityScore(pawn, castTarget.Cell, radius);
+            if (opportunityScore < MinimumOpportunityScore)
+            {
+                return;
+            }
+
+            float score = GetVanillaShootingTargetScore(attackTarget, pawn, verb, radius) + opportunityScore;
+            if (score > bestEvaluation.Score)
+            {
+                bestEvaluation = new ApparelVerbEvaluation(verb, castTarget, score);
+            }
+        }
+
+        private static bool TryGetCastTarget(Verb verb, Thing targetThing, out LocalTargetInfo castTarget)
+        {
+            LocalTargetInfo thingTarget = targetThing;
+            if (CanUseTarget(verb, thingTarget))
+            {
+                castTarget = thingTarget;
                 return true;
             }
 
-            if (vanillaTarget.Thing == null)
-            {
-                castTarget = LocalTargetInfo.Invalid;
-                return false;
-            }
-
-            LocalTargetInfo cellTarget = vanillaTarget.Thing.Position;
-            if (CanUseTarget(ability, verb, cellTarget))
+            LocalTargetInfo cellTarget = targetThing.Position;
+            if (CanUseTarget(verb, cellTarget))
             {
                 castTarget = cellTarget;
                 return true;
@@ -128,47 +140,27 @@ namespace EnemiesUseApparelToo
             return false;
         }
 
-        private static bool CanUseTarget(Ability ability, Verb verb, LocalTargetInfo target)
+        private static bool CanUseTarget(Verb verb, LocalTargetInfo target)
         {
             return target.IsValid &&
-                ability.AICanTargetNow(target) &&
                 verb.CanHitTarget(target) &&
                 verb.ValidateTarget(target, showMessages: false);
         }
 
-        private static float GetAoeOpportunityScore(Pawn pawn, Ability ability, LocalTargetInfo castTarget, float radius)
+        private static float GetAoeOpportunityScore(Pawn pawn, IntVec3 center, float radius)
         {
-            int hostileTargets = CountVanillaAffectedHostiles(pawn, ability, castTarget);
-            if (hostileTargets == 0)
-            {
-                hostileTargets = CountHostilesInRadius(pawn, ability, castTarget.Cell, radius);
-            }
-
-            int explosives = CountExplosivesInRadius(pawn, castTarget.Cell, radius);
+            int hostileTargets = CountHostilesInRadius(pawn, center, radius);
+            int explosives = CountExplosivesInRadius(pawn, center, radius);
             int extraHostileTargets = Math.Max(0, hostileTargets - 1);
             return extraHostileTargets * ExtraHostileTargetScore + explosives * ExplosiveTargetScore;
         }
 
-        private static int CountVanillaAffectedHostiles(Pawn pawn, Ability ability, LocalTargetInfo castTarget)
-        {
-            int hostileTargets = 0;
-            foreach (LocalTargetInfo affectedTarget in ability.GetAffectedTargets(castTarget))
-            {
-                if (affectedTarget.Thing is Pawn targetPawn && IsHostileAoePawn(pawn, targetPawn))
-                {
-                    hostileTargets++;
-                }
-            }
-
-            return hostileTargets;
-        }
-
-        private static int CountHostilesInRadius(Pawn pawn, Ability ability, IntVec3 center, float radius)
+        private static int CountHostilesInRadius(Pawn pawn, IntVec3 center, float radius)
         {
             int hostileTargets = 0;
             foreach (Thing thing in GenRadial.RadialDistinctThingsAround(center, pawn.Map, radius, useCenter: true))
             {
-                if (thing is Pawn targetPawn && IsHostileAoePawn(pawn, targetPawn) && IsValidAoeAffectedTarget(ability, targetPawn))
+                if (thing is Pawn targetPawn && IsHostileAoePawn(pawn, targetPawn))
                 {
                     hostileTargets++;
                 }
@@ -196,23 +188,6 @@ namespace EnemiesUseApparelToo
             return targetPawn.Spawned &&
                 !targetPawn.Downed &&
                 targetPawn.HostileTo(pawn);
-        }
-
-        private static bool IsValidAoeAffectedTarget(Ability ability, Thing target)
-        {
-            if (ValidAoeAffectedTargetMethod == null)
-            {
-                return true;
-            }
-
-            try
-            {
-                return (bool)ValidAoeAffectedTargetMethod.Invoke(ability, new object[] { target });
-            }
-            catch
-            {
-                return true;
-            }
         }
 
         private static bool IsExplosiveOpportunity(Pawn pawn, Thing thing)
@@ -257,36 +232,21 @@ namespace EnemiesUseApparelToo
             }
         }
 
-        private static bool TryGetAbility(Verb verb, out Ability ability)
+        private static bool TryGetExplosionRadius(Verb verb, out float radius)
         {
-            ability = (verb as Verb_CastAbility)?.Ability;
-            return ability != null;
-        }
-
-        private static bool TryGetExplosionRadius(Ability ability, out float radius)
-        {
-            radius = 0f;
-            CompProperties_AbilityLaunchProjectile launchProjectile =
-                ability.def.comps?.OfType<CompProperties_AbilityLaunchProjectile>().FirstOrDefault();
-
-            ThingDef projectileDef = launchProjectile?.projectileDef;
-            if (projectileDef?.projectile == null)
+            radius = verb.verbProps?.defaultProjectile?.projectile?.explosionRadius ?? 0f;
+            if (radius > 0f)
             {
-                return false;
+                return true;
             }
 
-            radius = projectileDef.projectile.explosionRadius;
+            radius = verb.verbProps?.ai_AvoidFriendlyFireRadius ?? 0f;
             return radius > 0f;
         }
 
         private static Job MakeApparelVerbJob(ApparelVerbEvaluation evaluation)
         {
-            Job job = evaluation.Ability.GetJob(evaluation.Target, evaluation.Target);
-            if (job == null)
-            {
-                return null;
-            }
-
+            Job job = JobMaker.MakeJob(JobDefOf.UseVerbOnThing, evaluation.Target);
             job.verbToUse = evaluation.Verb;
             job.maxNumStaticAttacks = 1;
             job.expiryInterval = 2000;
@@ -297,21 +257,19 @@ namespace EnemiesUseApparelToo
 
         private readonly struct ApparelVerbEvaluation
         {
-            public static readonly ApparelVerbEvaluation Invalid = new ApparelVerbEvaluation(null, null, LocalTargetInfo.Invalid, float.MinValue);
+            public static readonly ApparelVerbEvaluation Invalid = new ApparelVerbEvaluation(null, LocalTargetInfo.Invalid, float.MinValue);
 
-            public ApparelVerbEvaluation(Ability ability, Verb verb, LocalTargetInfo target, float score)
+            public ApparelVerbEvaluation(Verb verb, LocalTargetInfo target, float score)
             {
-                Ability = ability;
                 Verb = verb;
                 Target = target;
                 Score = score;
             }
 
-            public Ability Ability { get; }
             public Verb Verb { get; }
             public LocalTargetInfo Target { get; }
             public float Score { get; }
-            public bool IsValid => Ability != null && Verb != null && Target.IsValid;
+            public bool IsValid => Verb != null && Target.IsValid;
         }
     }
 }
